9070164c49060982be770c15ab81cab26508fc8d
Dmitry Rykov <dmitry_rykov@epam.com>
2017-10-11 17:13:06 +0300

    Implement `isDayHighlighted` for Calendar

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -1,13 +1,50 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import formatDate  from 'date-fns/format';
+import getISODay from 'date-fns/get_iso_day'
 
 import { setDate } from './../store/actions';
 
 import Calendar from './../components/calendar';
 
+const getIsNumerator = (date, isNumeratorOdd) => {
+    const isOdd = (0 + formatDate(date, 'W')) % 2 === 0;
+    return isOdd === isNumeratorOdd;
+}
+
 const mapStateToProps = (state) => ({
     date: state.date.date,
-    isDayHighlightedFn: (date) => false,
+    isDayHighlightedFn: (date) => {
+        if (!state.search.keyword) {
+            return false;
+        }
+        const isNum = getIsNumerator(date, state.date.isNumeratorOdd);
+        const schedule = isNum ? state.schedule.numerator : state.schedule.denominator;
+
+        if (!schedule) {
+            return false;
+        }
+
+        const dayLessons = schedule[+getISODay(date)];
+
+        if (!(dayLessons && dayLessons.length)) {
+            return false;
+        }
+
+        switch (state.search.type) {
+            case 'lector':
+                return dayLessons
+                    .filter(lesson => !!(lesson && lesson.lector))
+                    .reduce((acc, next) => [...acc, ...next.lector], [])
+                    .find(lector => lector === state.search.keyword);
+            case 'lesson':
+                return dayLessons
+                    .filter(lesson => !!(lesson && lesson.name))
+                    .reduce((acc, next) => [...acc, next.name], [])
+                    .find(name => name === state.search.keyword);
+        }
+        return false;
+    },
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
